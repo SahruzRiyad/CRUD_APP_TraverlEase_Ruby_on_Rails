9bae1d6676db51e2f045a6730dbf7fda3726f150
SahruzRiyad <sahruzzaman01@gmail.com>
2024-07-27 02:35:41 +0600

    Hotels CRUP operations added

--- app/controllers/hotel_controller.rb
+++ app/controllers/hotel_controller.rb
@@ -1,4 +1,77 @@
 class HotelController < ApplicationController
-  def search
-  end
+
+    def index
+        @hotels = Hotel.page(params[:page]).per(10)
+    end
+
+    def new
+
+    end
+
+    def create
+        @hotel = Hotel.new(hotel_params) 
+
+        if @hotel.save
+            flash[:success] = "Hotel Successfully added."
+            redirect_to show_hotel_index_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @hotel = Hotel.find_by(params[:id])
+    end
+
+    def update
+        @hotel = Hotel.find_by(params[:id])
+
+        if @hotel.update(hotel_params)
+            flash[:success] = "Hotel updated successfully!"
+            redirect_to show_hotel_index_path
+        else
+            render :edit
+        end
+    end
+
+
+    def destroy
+        @hotel = Hotel.find(params[:format])
+        @hotel.destroy
+        redirect_to show_hotel_index_path, notice: "Hotel successfully deleted."
+    end
+
+    def hotelSearch
+        if params[:query].present?
+            @hotels = search_hotels(params[:query]).page(params[:page]).per(10)
+            render :index
+        else 
+            redirect_to show_hotel_index_path
+        end
+    end
+
+    def search
+        @flights = Flight.where(
+        start: params[:from],
+        destination: params[:to],
+        )
+        render :results  # Render the index view to display results
+    end
+
+    private
+
+        def search_hotels(query)
+            query = query.downcase
+            @hotels = Hotel.where("LOWER(name) LIKE ? OR LOWER(location) LIKE ?", 
+                    "%#{query}%", 
+                    "%#{query}%")
+            return @hotels
+        end
+
+        def hotel_params
+            # Permitting the custom parameter names from your form
+            params.permit(:name, :location, :rating, :available_rooms, :price_per_night) 
+        end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   post '/flights/create', to: 'flights#create', as: 'add_flight'
   patch 'flights/update' , to: 'flights#update' , as: 'flight'
+
+  post '/hotel/create' , to: 'hotel#create' , as: 'add_hotel'
+  patch 'hotel/update' , to: 'hotel#update' , as: 'hotel_update'
   
   # Static pages
   get 'home/index'
@@ -19,8 +22,15 @@ Rails.application.routes.draw do
   get 'flights/edit' , to: 'flights#edit' , as: 'edit_flight'
   get 'flights/new' , to: 'flights#new' , as: 'get_new_flight'
 
+  get 'hotel/index', to: 'hotel#index' , as: 'show_hotel_index'
+  get '/hotel/hotelSearch' , to: 'hotel#hotelSearch' , as: 'search_hotels'
+  get 'hotel/new' , to: 'hotel#new' , as: 'add_new_hotel'
+  get 'hotel/edit', to: 'hotel#edit' , as: 'edit_hotel'
+
   
   delete 'flights/delete' , to: 'flights#destroy', as: 'delete_flight'
+
+  delete 'hotel/delete' , to: 'hotel#destroy' , as: 'delete_hotel'
   # Users resources with nested bookings
   # resources :users do
   #   resources :bookings, only: [:new, :create, :index, :show]
